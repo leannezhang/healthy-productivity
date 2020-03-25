1bb74cd5ceb2a04eb32d805feb166dd20bcaa7fe
Kiruthika Sivaraman <ksivaraman@twitter.com>
2020-03-24 18:56:12 -0700

    Added basic options page

--- kiru/src/js/options.js
+++ kiru/src/js/options.js
@@ -1,15 +1,73 @@
-let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
+let domFocusTime = document.getElementsByName("focus-time");
+let domBreakTime = document.getElementsByName("break-time");
+
+function setEventListeners() {
+  document.getElementById("options-form").addEventListener("submit", e => {
+    e.preventDefault();
+    saveOptions();
+  });
+
+  document.getElementById("reset-options").addEventListener("click", () => {
+    resetDefaults();
+  });
+
+  document.getElementById('fitness').addEventListener("click", e => {
+    var div = document.getElementById('fitness-activities');
+    toggleDivDisplay(div);
+  });
+
+  document.getElementById('meditation').addEventListener("click", e => {
+    var div = document.getElementById('meditation-activities');
+    toggleDivDisplay(div);
+  });
+
+  document.getElementById('stretching').addEventListener("click", e => {
+    var div = document.getElementById('stretching-activities');
+    toggleDivDisplay(div);
+  });
+}
+
+function toggleDivDisplay(div) {
+  if (div.style.display === 'none') {
+    div.style.display = 'block';
+  } else {
+    div.style.display = 'none';
+  }
+}
+
+function resetDefaults() {
+  document.getElementById("one-hr").checked = true;
+  document.getElementById("10-mins").checked = true;
+  document.getElementById("fitness").checked = false;
+  document.getElementById("meditation").checked = false;
+  document.getElementById("stretching").checked = false;
+  document.getElementById('fitness-activities').style.display = 'none';
+  document.getElementById('meditation-activities').style.display = 'none';
+  document.getElementById('stretching-activities').style.display = 'none';
+}
+
+function saveOptions() {
+
+  const focusTime = getCheckedValue(domFocusTime);
+  chrome.storage.sync.set({focusTime: focusTime}, function() {
+   console.log('focusTime is ' + focusTime);
+  });
+
+  const breakTime = getCheckedValue(domBreakTime);
+  chrome.storage.sync.set({breakTime: breakTime}, function() {
+   console.log('breakTime is ' + breakTime);
+  });
+
+  //Need to store fav activities
+
+}
+
+function getCheckedValue(radioElement) {
+  for(var i = 0; i < radioElement.length; i++){
+    if(radioElement[i].checked){
+      return radioElement[i].value
     }
   }
-  constructOptions(kButtonColors);
+}
+
+setEventListeners();
