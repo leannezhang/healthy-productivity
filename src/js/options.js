6bea2f10c16e61e0c0bc9c31c4a3669e408316ab
Liyang Zhang <liyangz@twitter.com>
2021-03-15 10:43:30 -0700

    more cleaned up

--- src/js/options.js
+++ src/js/options.js
@@ -1,16 +1,15 @@
-let domActivities = document.getElementsByName("activities");
 class UserProfile {
   constructor(
     gender = "",
     age = "",
     activityImpact = "",
     goal = "",
-    equipments = []) {
+    equipment = "") {
     this.gender = gender;
     this.age = age;
     this.activityImpact = activityImpact;
     this.goal = goal;
-    this.equipments = equipments;
+    this.equipment = equipment;
   }
 
   read() {
@@ -23,7 +22,7 @@ class UserProfile {
       this.age = userProfile.age;
       this.activityImpact = userProfile.activityImpact;
       this.goal = userProfile.goal;
-      this.equipments = userProfile.equipments;
+      this.equipment = userProfile.equipment;
     }
   }
 
@@ -33,28 +32,25 @@ class UserProfile {
 
   write() {
     console.log("Writing UserProfile")
-
     let userProfileJson = {
       gender: "",
       age: "",
-      activityImpact: [],
+      activityImpact: "",
       goal: "",
-      equipments: [],
+      equipment: "",
     }
     userProfileJson.gender = this.gender;
     userProfileJson.age = this.age;
     userProfileJson.activityImpact = this.activityImpact;
     userProfileJson.goal = this.goal;
-    userProfileJson.equipments = this.equipments;
-    let stringified = JSON.stringify(userProfileJson);
-
-    console.log("userProfileJson: ", stringified, userProfileJson);
-    return localStorage.setItem("userProfile", stringified);
+    userProfileJson.equipment = this.equipment;
+    console.log("userProfileJson: ", JSON.stringify(userProfileJson));
+    return localStorage.setItem("userProfile", JSON.stringify(userProfileJson));
   }
 }
 
 let userProfile = new UserProfile();
-function readFromLocalStorage() {
+function prepopulateDataFromStorage() {
   /*
     Load UserProfile from localStorage
     pre-populate html fields
@@ -67,10 +63,8 @@ function readFromLocalStorage() {
   let age = document.querySelector("input[id=age-input]")
   let highImpactActivity = document.querySelector("input[id=activity-high-impact]")
   let lowImpactActivity = document.querySelector("input[id=activity-low-impact]")
-  let goalsValue = document.querySelector("select[id=goals]")
-
-  // TODO (liyangz): need to make the field checkbox
-  let equipmentsValue = document.querySelector("select[id=equipments]").value
+  let goalValue = document.querySelector("select[id=goal]")
+  let equipmentValue = document.querySelector("select[id=equipment]")
 
   // Assign input element
   switch (userProfile.gender) {
@@ -96,26 +90,27 @@ function readFromLocalStorage() {
       break;
     }
   }
-  goalsValue = userProfile.goal
+  goalValue = userProfile.goal;
+  equipmentValue = userProfile.equipment;
 }
 
 // collect all input and store them
-function collectFields() {
+function saveUserProfile() {
   let gender = document.querySelector('input[name=gender]:checked').value
   let age = document.querySelector("input[name=age]").value;
   let activityImpact = document.querySelector('input[name=activity-impact]:checked').value;
-  let goalsValue = document.querySelector("select[id=goal]").value
+  let goalValue = document.querySelector("select[id=goal]").value
   // load everything
   // TODO (liyangz) fix equipment. pass in an array
-  userProfile = new UserProfile(gender, age, activityImpact, goalsValue, [])
+  userProfile = new UserProfile(gender, age, activityImpact, goalValue, [])
   userProfile.write()
 }
 
 function setEventListeners() {
   document.getElementById("options-form").addEventListener("submit", e => {
     e.preventDefault();
-    collectFields()
-    saveOptions();
+    saveUserProfile();
+    saveTimeOptions();
   });
 
   document.getElementById("reset-options").addEventListener("click", () => {
@@ -129,6 +124,7 @@ function setEventListeners() {
     input.value = count;
     return false;
   });
+
   document.getElementById("focus-time-plus").addEventListener("click", e => {
     var input = document.getElementById("focus-time");
     var count = parseInt(input.value) + 1;
@@ -143,7 +139,8 @@ function setEventListeners() {
     count = count < 1 ? 1 : count;
     input.value = count;
     return false;
-  });
+  })
+  
   document.getElementById("break-time-plus").addEventListener("click", e => {
     var input = document.getElementById("break-time");
     var count = parseInt(input.value) + 1;
@@ -153,27 +150,14 @@ function setEventListeners() {
   });
 }
 
-function addListElement(id, text) {
-  var li = document.createElement("li");
-  li.innerHTML = text;
-  document.getElementById(id).appendChild(li);
-}
-
+// TODO: reset defaults
 function resetDefaults() {
   document.getElementById("focus-time").value = 30;
   document.getElementById("break-time").value = 5;
   document.getElementById("fitness").checked = false;
-  document.getElementById("meditation").checked = false;
-  document.getElementById("stretching").checked = false;
-  document.getElementById("fitness-activities").style.display = "none";
-  document.getElementById("meditation-activities").style.display = "none";
-  document.getElementById("stretching-activities").style.display = "none";
-  document.getElementById("fitness-list").innerHTML = "";
-  document.getElementById("meditation-list").innerHTML = "";
-  document.getElementById("stretching-list").innerHTML = "";
 }
 
-function saveOptions() {
+function saveTimeOptions() {
   const focusTime = document.getElementById("focus-time").value;
   chrome.storage.sync.set({ focusTime: focusTime }, function () {
     console.log("focusTime is " + focusTime);
@@ -183,39 +167,9 @@ function saveOptions() {
   chrome.storage.sync.set({ breakTime: breakTime }, function () {
     console.log("breakTime is " + breakTime);
   });
-
-  const activityCategory = getCheckedValue(domActivities);
-
-  if (typeof activityCategory !== "undefined") {
-    chrome.storage.sync.set({ activityCategory: activityCategory }, function () {
-      console.log("Selected activity is " + activityCategory);
-    });
-
-    var activityLst = [];
-    const lstNodes = document
-      .getElementById(activityCategory + "-list")
-      .getElementsByTagName("li");
-    for (let i = 0; i < lstNodes.length; i++) {
-      activityLst.push(lstNodes[i].textContent);
-    }
-
-    //    const activities = document.getElementById(activityCategory + "-activities-lst").value;
-    //    const activityLst = activities.split(/[ ,\n]/);
-    chrome.storage.sync.set({ activityLst: activityLst }, function () {
-      console.log("Provided activity list is " + activityLst);
-    });
-  }
 }
 
-function getCheckedValue(radioElement) {
-  for (var i = 0; i < radioElement.length; i++) {
-    if (radioElement[i].checked) {
-      return radioElement[i].value;
-    }
-  }
-}
-
-readFromLocalStorage();
+prepopulateDataFromStorage();
 setEventListeners();
 
 
